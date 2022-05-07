d2fef4047ff97204de50990e6afd83ed42a7c4aa
Nikhil Khandelwal <nikhilkhandelwal2021@u.northwestern.edu>
2022-05-06 23:31:09 -0500

    Fixing header size issue.

--- lab06/01-font-switcher/index.js
+++ lab06/01-font-switcher/index.js
@@ -1,15 +1,30 @@
 const makeBigger = () => {
    var element = document.getElementsByClassName("content")[0];
+   var header = document.querySelector("header");
+
    var currFont = window.getComputedStyle(element,null).getPropertyValue('font-size');
+   console.log(header)
+   var headerFont = window.getComputedStyle(header,null).getPropertyValue('font-size');
+
    var fontSize = parseFloat(currFont); 
+   var headerSize = parseFloat(headerFont); 
    element.style.fontSize = (fontSize + 2) + "px";
+   header.style.fontSize = (headerSize + 2) + "px";
 };
 
 const makeSmaller = () => {
    var element = document.getElementsByClassName("content")[0];
+   var header = document.querySelector("header");
+
    var currFont = window.getComputedStyle(element,null).getPropertyValue('font-size');
+   var headerFont = window.getComputedStyle(header,null).getPropertyValue('font-size');
+
    var fontSize = parseFloat(currFont); 
+   var headerSize = parseFloat(headerFont); 
+
    element.style.fontSize = (fontSize - 2) + "px";
+   header.style.fontSize = (headerSize - 2) + "px";
+
 };
 
 
